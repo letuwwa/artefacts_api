c5398a57dacf7a07a450fb008f85cbad99f2b63f
Litvinenka <letuwwa@gmail.com>
2022-09-04 17:30:09 +0400

    feat: artefact model

--- artefacts/admin.py
+++ artefacts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from artefacts.models import Artefact
 
-# Register your models here.
+
+admin.site.register(Artefact)
--- artefacts/models.py
+++ artefacts/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Artefact(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=128, null=False, help_text="Name")
+    description = models.CharField(max_length=2048, null=True, help_text="Description")
+
+    def __str__(self):
+        return f"{self.id}"
+
+    class Meta:
+        verbose_name = "Artefact"
+        verbose_name_plural = "Artefacts"
