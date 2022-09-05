fb1c31320c295dd0536406398cf6430cd48f074b
Litvinenka <letuwwa@gmail.com>
2022-09-05 09:57:00 +0400

    feat: test url

---
+++ artefacts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ArtefactView
+from .apps import ArtefactsConfig
+
+
+app_name = ArtefactsConfig.name
+
+urlpatterns = [
+    path("", ArtefactView.as_view())
+]
