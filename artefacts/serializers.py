45dc99a197984f28c700068f3c34eb2eeb916e5c
Litvinenka <letuwwa@gmail.com>
2022-09-23 22:03:00 +0400

    feat: ArcheologistSerializer

--- artefacts/serializers.py
+++ artefacts/serializers.py
@@ -1,8 +1,14 @@
-from artefacts.models import Artefact
+from artefacts.models import Artefact, Archeologist
 from rest_framework.serializers import ModelSerializer
 
 
 class ArtefactSerializer(ModelSerializer):
     class Meta:
         model = Artefact
+        fields = "__all__"
+
+
+class ArcheologistSerializer(ModelSerializer):
+    class Meta:
+        model = Archeologist
         fields = "__all__"
