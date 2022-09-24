b49f648d6324738ef4d3493b4fbdfff4b3146873
Litvinenka <letuwwa@gmail.com>
2022-09-24 17:51:42 +0400

    feat: migrations and user model

--- auth_api/models.py
+++ auth_api/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=128, null=False)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128, null=False)
+    username = None
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
--- core/settings.py
+++ core/settings.py
@@ -91,10 +91,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -103,13 +99,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = "static/"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "auth_api.User"
