28cd9eb274baa32156ac5affbc3815e842970ca2
Litvinenka <letuwwa@gmail.com>
2022-09-24 18:27:17 +0400

    feat: password hashing

--- auth_api/serializers.py
+++ auth_api/serializers.py
@@ -3,6 +3,14 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(**validated_data)
+        if password:
+            instance.set_password(raw_password=password)
+        instance.save()
+        return instance
+
     class Meta:
         model = User
         fields = ("id", "first_name", "email", "password")
