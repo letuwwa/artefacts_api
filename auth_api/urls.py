51ffdd68a92e743a4247dc65f48abebbdff25227
Litvinenka <letuwwa@gmail.com>
2022-09-24 18:57:51 +0400

    feat: LoginView

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from auth_api.views import RegisterView
+from auth_api.views import RegisterView, LoginView
 
 
 urlpatterns = [
+    path("login/", LoginView.as_view()),
     path("register/", RegisterView.as_view()),
 ]
--- auth_api/views.py
+++ auth_api/views.py
@@ -1,14 +1,33 @@
 from rest_framework.views import APIView
 from rest_framework.request import Request
+from rest_framework.response import Response
 
+from auth_api.models import User
 from auth_api.serializers import UserSerializer
 from artefacts.base.base_response_class import BaseResponse
 
 
 class RegisterView(APIView, BaseResponse):
-    def post(self, request: Request):
+    def post(self, request: Request) -> Response:
         user_serializer = UserSerializer(data=request.data)
         if user_serializer.is_valid():
             user_serializer.save()
             return self.get_response_created(value=user_serializer.data)
         return self.get_response_bad_request(value=user_serializer.errors)
+
+
+class LoginView(APIView, BaseResponse):
+    def post(self, request: Request) -> Response:
+        if not (email := request.data.get("email")):
+            return self.get_response_bad_request()
+
+        if not (password := request.data.get("password")):
+            return self.get_response_bad_request()
+
+        if not (user := User.objects.filter(email=email).first()):
+            return self.get_response_forbidden()
+
+        if not user.check_password(raw_password=password):
+            return self.get_response_forbidden()
+
+        return self.get_response_ok()
