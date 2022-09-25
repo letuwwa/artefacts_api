3c36e23aaeecc788e51a5834af3769117fc2e4ca
Litvinenka <letuwwa@gmail.com>
2022-09-25 14:29:37 +0400

    fix: urls

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("artefacts.urls"))]
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("artefacts/", include("artefacts.urls")),
+]
