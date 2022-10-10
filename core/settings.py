564f7daa747cf14abcdcecc49c9213e712a9dd59
Litvinenka <letuwwa@gmail.com>
2022-10-10 19:12:38 +0400

    feat: blacklist token settings

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "artefacts_api",
     "auth_api",
