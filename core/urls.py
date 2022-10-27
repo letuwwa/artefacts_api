84c6bf16f027d95507ffccc4ea9c9c8bfc7b05b4
Litvinenka <letuwwa@gmail.com>
2022-10-27 13:26:47 +0400

    feat: root view

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(["GET"])
+def api_root(request: Request):
+    return Response(f"Hi!, {request.user}")
+
 
 urlpatterns = [
+    path("", api_root),
     path("admin/", admin.site.urls),
     path("auth_api/", include("auth_api.urls")),
     path("artefacts_api/", include("artefacts_api.urls")),
