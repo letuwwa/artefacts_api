d5901a3d4805b606830c0fafe9795eb4150f0715
Maksim Litvinenka <letuwwa@gmail.com>
2023-01-15 14:37:52 +0400

    feat: swagger

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,36 @@
+from drf_yasg import openapi
 from django.contrib import admin
-from django.urls import path, include
-
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
 
 
-@api_view(["GET"])
-def api_root(request: Request):
-    return Response(f"Hi!, {request.user}")
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Jaseci API",
+        default_version="v1",
+        description="Welcome to the world of Jaseci",
+        terms_of_service="https://www.jaseci.org",
+        contact=openapi.Contact(email="jason@jaseci.org"),
+        license=openapi.License(name="Awesome IP"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 
 urlpatterns = [
-    path("", api_root),
+    re_path(
+        r"^doc(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "doc/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("admin/", admin.site.urls),
     path("auth_api/", include("auth_api.urls")),
     path("artefacts_api/", include("artefacts_api.urls")),
