afd5f01f7e859de903f564230e1eead6241e158b
Maksim Litvinenka <letuwwa@gmail.com>
2023-01-17 20:14:34 +0400

    feat: convert unique values to lowercase

--- auth_api/serializers.py
+++ auth_api/serializers.py
@@ -4,17 +4,29 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class CustomObtainPairSerializer(TokenObtainPairSerializer):
-    """
-        Saving username to token
-    """
     @classmethod
     def get_token(cls, user: User):
+        """
+        Saving username to token
+        """
         token = super().get_token(user)
         token["username"] = user.username
         return token
 
 
 class UserSerializer(serializers.ModelSerializer):
+    def validate_email(self, value: str) -> str:
+        """
+        Convert email to lowercase
+        """
+        return value.lower()
+
+    def validate_username(self, value: str) -> str:
+        """
+        Convert username to lowercase
+        """
+        return value.lower()
+
     def create(self, validated_data):
         password = validated_data.pop("password", None)
         instance = self.Meta.model(**validated_data)
